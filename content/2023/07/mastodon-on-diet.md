f345e36501e4f54dc7ea2d502b9becccd668f7a6
Harshad Sharma <harshad@sharma.io>
2023-07-04 13:05:39 +0530

    Add pelican blog

---
+++ content/2023/07/mastodon-on-diet.md
@@ -0,0 +1,7 @@
+Title: Mastodon On Diet
+Date: 2023-07-04
+Category: Mastodon
+
+How to put your Mastodon instance on a diet and reduce the storage bloat.
+
+#TODO
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = "Harshad Sharma"
+SITENAME = "Harshad's Blog"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Asia/Kolkata"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Social", "https://mastodon.sharma.io/@harshad/"),
+    ("Software", "https://github.com/hiway/"),
+)
+
+# Social widget
+# SOCIAL = (
+#     ("You can add links in your config file", "#"),
+#     ("Another social link", "#"),
+# )
+
+DEFAULT_PAGINATION = 20
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
